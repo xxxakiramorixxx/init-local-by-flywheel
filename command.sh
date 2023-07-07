7201973e8572fdaf0d03545016386cd490a44eda
akiramori <102807866+xxxakiramorixxx@users.noreply.github.com>
2023-07-07 17:36:10 +0900

    Update command.sh

--- command.sh
+++ command.sh
@@ -37,7 +37,7 @@ wp --allow-root plugin install admin-bar-position --activate
 wp --allow-root plugin install classic-editor --activate
 wp --allow-root plugin install akismet --activate
 wp --allow-root plugin install broken-link-checker --activate
-wp --allow-root plugin install post-types-order --activate
+wp --allow-root plugin install intuitive-custom-post-order --activate
 
 # Mailhog メールが文字化けしないよう WP Multibyte Patch の設定調整
 # https://eastcoder.com/code/wp-multibyte-patch/
